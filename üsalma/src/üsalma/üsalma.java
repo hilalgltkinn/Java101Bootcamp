bbfbe29b39c40bfa81c363992fdf936e478cadae
= <80570316+hilalgltkinn@users.noreply.github.com>
2021-06-24 02:52:37 +0300

    üsalma

---
+++ üsalma/src/üsalma/üsalma.java
@@ -0,0 +1,25 @@
+package üsalma;
+
+ 
+import java.util.Scanner;
+ 
+public class üsalma {
+    
+    static int usalma(int sayi1, int sayi2) {
+        int sonuc =(int) Math.pow(sayi1, sayi2);
+        return sonuc;
+        
+    }
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("1. Sayiyi giriniz: ");
+        int sayi1 = scan.nextInt();
+        System.out.print("2. Sayiyi giriniz: ");
+        int sayi2 = scan.nextInt();
+        
+        int sonuc = usalma(sayi1,sayi2);
+        System.out.println(sayi1 + "^" + sayi2 + " = " + sonuc);
+    }
+}
