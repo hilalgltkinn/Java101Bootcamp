53fdb3573187e473488f14adbf48aa6a9ae0a90b
= <80570316+hilalgltkinn@users.noreply.github.com>
2021-06-24 03:52:15 +0300

    ücgen

---
+++ ücgen/src/ücgen/ücgen.java
@@ -0,0 +1,31 @@
+package ücgen;
+
+import java.util.Scanner;
+
+public class ücgen{
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        
+        Scanner klavye=new Scanner(System.in);
+        
+        System.out.println("Bir Sayý Giriniz");
+        int n=klavye.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }   
+            for(int k=2*n-1;k>=(2*i+1);k--){
+                System.out.print("*");
+                
+            }
+                System.out.println();   
+
+        }
+        
+        
+    }
+
+}
