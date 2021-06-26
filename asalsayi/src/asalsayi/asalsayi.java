4c470b593df68f357986a934f225449156d47863
= <80570316+hilalgltkinn@users.noreply.github.com>
2021-06-26 23:09:46 +0300

    asalsayi

--- asalsayi/src/asalsayi/asalsayi.java
+++ asalsayi/src/asalsayi/asalsayi.java
@@ -1,28 +1,35 @@
 package asalsayi;
 
-public class asalsayi{
- 
-public static void main(String[] args) {
-    
+
+import java.util.Scanner;
+
+public class asalsayi {
+
+    public static void main(String[] args) {
+        System.out.println("Bir sayi giriniz");
+        Scanner tara=new Scanner(System.in);
+        int sayi=tara.nextInt();
         int sayac = 0;
-        for(int sayi=2;sayi<=100;sayi++)
-        {
-            int kontrol = 0;
-            for (int i = 2; i < sayi; i++)
-            {
-               if (sayi % i == 0)
-                {
-                    kontrol = 1;
-                    break;
-                } 
-            }
-                
-            if(kontrol==0)
-            {
-                System.out.print(sayi+"\n");
-                sayac++; 
-            }
+        if(sayi<1){
+            System.out.println("1 den buyuk sayilar, asal sayi olabilir");
         }
-    }    
+        else{
+            for(int i=1;i<=sayi;i++){
+                int kalan=sayi%i;
+                if(kalan==0){
+                sayac++;
+            }
+            }
+                if(sayac==2){
+                    System.out.println("Sayi asaldir");
+                }
+                else{
+                    System.out.println("Sayi asal degildir");
+                }
+        }
+    }
 }
- 
+        
+
+    
+
