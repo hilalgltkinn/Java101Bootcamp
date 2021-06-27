9ad9a7b74b34df40e021408995c0e8b2f41d8bd5
= <80570316+hilalgltkinn@users.noreply.github.com>
2021-06-27 13:24:43 +0300

    havasicakligi

---
+++ havasicakligi/src/havasicakligi/havasicakligi.java
@@ -0,0 +1,20 @@
+package havasicakligi;
+
+import java.util.Scanner;
+
+public class havasicakligi {
+	public static void main(String[] args) {
+		  Scanner inp = new Scanner(System.in);
+	        System.out.print("Sýcaklýk derecesini giriniz: ");
+	       int heat = inp.nextInt();
+	        if (heat<=5){
+	            System.out.println("Kayak");
+	        }else if(heat>5 || heat<=15){
+	            System.out.println("Sinema");
+	        }else if(heat>10 || heat<=25){
+	            System.out.println("Piknik");
+	        }else if(heat>25){
+	            System.out.println("Yüzme");
+	        }
+	    }
+	}
