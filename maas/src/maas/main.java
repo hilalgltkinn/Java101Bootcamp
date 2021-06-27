1be555c218b5b4937596d192430ed63b11c71106
= <80570316+hilalgltkinn@users.noreply.github.com>
2021-06-27 10:20:20 +0300

    maas

---
+++ maas/src/maas/main.java
@@ -0,0 +1,12 @@
+package maas;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		employee employee =new employee ("hilal",5000,20,2021);
+	     System.out.println(employee.toString());
+        
+	}
+
+}
