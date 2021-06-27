2839a23bcb12d1807b2318559ef7da16e6a293dd
= <80570316+hilalgltkinn@users.noreply.github.com>
2021-06-27 13:51:10 +0300

    artikyil

---
+++ artikyil/src/artikyil/artikyil.java
@@ -0,0 +1,30 @@
+package artikyil;
+
+public class artikyil {
+  
+    public static void main(String[] args) {
+        
+        int yil = 2021;
+        boolean artik = false;
+ 
+        if(yil % 4 == 0)
+        {
+            if( yil % 100 == 0)
+            {
+                if ( yil % 400 == 0)
+                    artik = true;
+                else
+                    artik = false;
+            }
+            else
+                artik = true;
+        }
+        else
+            artik = false;
+ 
+        if(artik)
+            System.out.println(yil + " artık yıldır.");
+        else
+            System.out.println(yil + " artık yıl değildir.");
+    } 
+}
